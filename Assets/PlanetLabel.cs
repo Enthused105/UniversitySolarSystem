ab5f4a3a2f2ede009b18dc8ef1c5876c37def902
enthused105 <ashwin.banarsee@gmail.com>
2020-02-28 08:26:09 +0000

    Added gas giants and trails to each planet.

--- Assets/PlanetLabel.cs
+++ Assets/PlanetLabel.cs
@@ -5,14 +5,11 @@ using UnityEngine.UI;
 public class PlanetLabel:MonoBehaviour
 {
     private Text label;
-
-    public string name;
     public GravityBody followTarget;
 
     public void Awake()
     {
         label = GetComponent<Text>();
-        label.text = name;
     }
 
     public void FixedUpdate()
