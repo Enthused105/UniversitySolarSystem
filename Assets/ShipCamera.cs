e50c4a585cecc59f20f62f2f746bf5e9aefd749d
enthused105 <ashwin.banarsee@gmail.com>
2020-03-19 22:13:09 +0000

    ShipController and ShipCamera

---
+++ Assets/ShipCamera.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+
+public class ShipCamera : CameraMode
+{
+    public ShipController ship;
+    public float distance;
+
+    public void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position,
+            ship.transform.position - ship.transform.forward * distance, 0.1f);
+        transform.LookAt(ship.transform.position);
+    }
+}
