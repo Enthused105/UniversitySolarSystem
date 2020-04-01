0240bd8fcb0f2454a35e4d4734539270588bcd3c
Enthused105 <60670071+Enthused105@users.noreply.github.com>
2020-04-01 21:48:22 +0100

    Added final files

---
+++ Scripts/MenuScreen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class MenuScreen : MonoBehaviour
+{
+
+	public void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+}
